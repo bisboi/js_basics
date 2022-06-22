9aedd6edac3fdf4bac4b96f30b877081c22c3dd9
bis_ <bence.i.solti@gmail.com>
2022-06-22 17:03:20 +0200

    5

--- frontend/script.js
+++ frontend/script.js
@@ -3,9 +3,16 @@ console.log('hello világ');
 function loadEvent() {
     console.log('loaded');  
 
-    document.getElementById('root').innerHTML='Sziasztok!'
-    console.dir(document.getElementById('root'))
-    console.dir(document.querySelector('#root'))
+let rootElement=document.getElementById('root')
+
+   rootElement.innerHTML='Sziasztok!'
+    //console.dir(document.getElementById('root'))
+    //console.dir(document.querySelector('#root'))
+    
+    rootElement.innerHTML+='Ciao'
+
+rootElement.insertAdjacentElementHTML('beforeend', 
+<h1>This is the title</h1>)
 }
 
 window.addEventListener('load',loadEvent)
